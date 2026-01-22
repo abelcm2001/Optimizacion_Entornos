a3dadb5acdb096e0ef3aa3615cc4dc5e04522f3c
abelcm2001 <abelcm2001@alumnos.ilerna.com>
2026-01-22 19:02:25 +0100

    He hecho cambios en el código, mediante un rename(Alt+shift+M) de las letras a,b,t y res

--- Trabajoproyecto/src/main/java/main/ProcesadorPedidos.java
+++ Trabajoproyecto/src/main/java/main/ProcesadorPedidos.java
@@ -9,29 +9,29 @@ import java.util.ArrayList;
 public class ProcesadorPedidos {
  
  
-    public double procesar(ArrayList<String> a, ArrayList<Double> b) {
-        double t = 0;
+    public double procesar(ArrayList<String> listaNombres, ArrayList<Double> precios) {
+        double total = 0;
         
         // Sumar precios de la lista
-        for (int i = 0; i < b.size(); i++) {
-            System.out.println("Añadiendo producto: " + a.get(i));
-            t = t + b.get(i);
+        for (int i = 0; i < precios.size(); i++) {
+            System.out.println("Añadiendo producto: " + listaNombres.get(i));
+            total = total + precios.get(i);
         }
         
         // Lógica de descuento (Magic Number 100 y 0.10)
-        if (t > 100) {
+        if (total > 100) {
             System.out.println("Descuento aplicado.");
-            t = t - (t * 0.10); 
+            total = total - (total * 0.10); 
         }
         
         // Cálculo de impuestos (Magic Number 0.21)
-        double res = t + (t * 0.21);
+        double resultado = total + (total * 0.21);
         
         // Gastos de envío (Magic Number 500 y 15.95)
-        if (res < 500) {
-            res = res + 15.95;
+        if (resultado < 500) {
+            resultado = resultado + 15.95;
         }
         
-        return res;
+        return resultado;
     }
 }
